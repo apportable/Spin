bb03992c432331aa45fb3f182f06089feab079f2
Aaron Culliney <asc@apportable.com>
2014-01-17 15:39:22 -0800

    A reachability test project

--- Spin/SpinViewController.m
+++ Spin/SpinViewController.m
@@ -6,9 +6,28 @@
 //
 
 #import "SpinViewController.h"
-#import <QuartzCore/QuartzCore.h>
+#import "Reachability.h"
+
+#import <netinet/in.h>
+#import <SystemConfiguration/SCNetworkReachability.h>
 
 @interface SpinViewController ()
+
+@property (retain, nonatomic) IBOutlet UILabel *summaryLabel;
+
+@property (retain, nonatomic) IBOutlet UITextField *remoteHostLabel;
+@property (retain, nonatomic) IBOutlet UIImageView *remoteHostImageView;
+@property (retain, nonatomic) IBOutlet UITextField *remoteHostStatusField;
+
+@property (retain, nonatomic) IBOutlet UIImageView *internetConnectionImageView;
+@property (retain, nonatomic) IBOutlet UITextField *internetConnectionStatusField;
+
+@property (retain, nonatomic) IBOutlet UIImageView *localWiFiConnectionImageView;
+@property (retain, nonatomic) IBOutlet UITextField *localWiFiConnectionStatusField;
+
+@property (retain, nonatomic) Reachability *hostReachability;
+@property (retain, nonatomic) Reachability *internetReachability;
+@property (retain, nonatomic) Reachability *wifiReachability;
 
 @end
 
@@ -23,14 +42,54 @@
     return self;
 }
 
+- (void)dealloc
+{
+    [_remoteHostLabel release];
+    [_remoteHostImageView release];
+    [_remoteHostStatusField release];
+    [_internetConnectionImageView release];
+    [_internetConnectionStatusField release];
+    [_localWiFiConnectionImageView release];
+    [_localWiFiConnectionStatusField release];
+    [_summaryLabel release];
+    
+    [_hostReachability release];
+    [_internetReachability release];
+    [_wifiReachability release];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:kReachabilityChangedNotification object:nil];
+    [super dealloc];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    CADisplayLink *link = [CADisplayLink displayLinkWithTarget:self.view selector:@selector(render:)];
-    link.frameInterval = 1;
-    [link addToRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
 	// Do any additional setup after loading the view.
     [self becomeFirstResponder];
+    
+    self.summaryLabel.hidden = YES;
+    
+    /*
+     Observe the kNetworkReachabilityChangedNotification. When that notification is posted, the method reachabilityChanged will be called.
+     */
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(reachabilityChanged:) name:kReachabilityChangedNotification object:nil];
+    
+    //Change the host name here to change the server you want to monitor.
+    NSString *remoteHostName = @"www.apple.com";
+    NSString *remoteHostLabelFormatString = NSLocalizedString(@"Remote Host: %@", @"Remote host label format string");
+    self.remoteHostLabel.text = [NSString stringWithFormat:remoteHostLabelFormatString, remoteHostName];
+    
+    self.hostReachability = [Reachability reachabilityWithHostName:remoteHostName];
+    [self.hostReachability startNotifier];
+    [self updateInterfaceWithReachability:self.hostReachability];
+    
+    self.internetReachability = [Reachability reachabilityForInternetConnection];
+    [self.internetReachability startNotifier];
+    [self updateInterfaceWithReachability:self.internetReachability];
+    
+    self.wifiReachability = [Reachability reachabilityForLocalWiFi];
+    [self.wifiReachability startNotifier];
+    [self updateInterfaceWithReachability:self.wifiReachability];
 }
 
 - (void)didReceiveMemoryWarning
@@ -41,15 +100,98 @@
 
 - (BOOL)shouldAutorotate
 {
-    return YES;
+    return NO;
 }
 
 - (NSUInteger)supportedInterfaceOrientations
 {
-    return UIInterfaceOrientationMaskAll;
+    return UIInterfaceOrientationPortrait;
 }
 
-//Back button support
+- (void)reachabilityChanged:(NSNotification *)note
+{
+    NSLog(@"SpinViewController noticed that the REACHABILITY CHANGED!!!");
+	Reachability *curReach = [note object];
+	NSParameterAssert([curReach isKindOfClass:[Reachability class]]);
+	[self updateInterfaceWithReachability:curReach];
+}
+
+- (void)updateInterfaceWithReachability:(Reachability *)reachability
+{
+    if (reachability == self.hostReachability)
+	{
+		[self configureTextField:self.remoteHostStatusField imageView:self.remoteHostImageView reachability:reachability];
+        NetworkStatus netStatus = [reachability currentReachabilityStatus];
+        BOOL connectionRequired = [reachability connectionRequired];
+        
+        self.summaryLabel.hidden = (netStatus != ReachableViaWWAN);
+        NSString* baseLabelText = @"";
+        
+        if (connectionRequired)
+        {
+            baseLabelText = NSLocalizedString(@"Cellular data network is available.\nInternet traffic will be routed through it after a connection is established.", @"Reachability text if a connection is required");
+        }
+        else
+        {
+            baseLabelText = NSLocalizedString(@"Cellular data network is active.\nInternet traffic will be routed through it.", @"Reachability text if a connection is not required");
+        }
+        self.summaryLabel.text = baseLabelText;
+    }
+    
+	if (reachability == self.internetReachability)
+	{
+		[self configureTextField:self.internetConnectionStatusField imageView:self.internetConnectionImageView reachability:reachability];
+	}
+    
+	if (reachability == self.wifiReachability)
+	{
+		[self configureTextField:self.localWiFiConnectionStatusField imageView:self.localWiFiConnectionImageView reachability:reachability];
+	}
+}
+
+- (void)configureTextField:(UITextField *)textField imageView:(UIImageView *)imageView reachability:(Reachability *)reachability
+{
+    NetworkStatus netStatus = [reachability currentReachabilityStatus];
+    BOOL connectionRequired = [reachability connectionRequired];
+    NSString* statusString = @"";
+    
+    switch (netStatus)
+    {
+        case NotReachable:
+        {
+            statusString = NSLocalizedString(@"Access Not Available", @"Text field text for access is not available");
+            imageView.image = [UIImage imageNamed:@"stop-32.png"] ;
+            /*
+             Minor interface detail- connectionRequired may return YES even when the host is unreachable. We cover that up here...
+             */
+            connectionRequired = NO;
+            break;
+        }
+        case ReachableViaWWAN:
+        {
+            statusString = NSLocalizedString(@"Reachable WWAN", @"");
+            imageView.image = [UIImage imageNamed:@"WWAN5.png"];
+            break;
+        }
+        case ReachableViaWiFi:
+        {
+            statusString= NSLocalizedString(@"Reachable WiFi", @"");
+            imageView.image = [UIImage imageNamed:@"Airport.png"];
+            break;
+        }
+    }
+    
+    if (connectionRequired)
+    {
+        NSString *connectionRequiredFormatString = NSLocalizedString(@"%@, Connection Required", @"Concatenation of status string with connection requirement");
+        statusString= [NSString stringWithFormat:connectionRequiredFormatString, statusString];
+    }
+    
+    textField.text= statusString;
+}
+
+
+// Android Back button support
 #ifdef APPORTABLE
 
 - (BOOL) canBecomeFirstResponder
