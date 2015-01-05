043e2e2361690a72505244dcabee45424f1827b2
Paul Beusterien <paulb@apportable.com>
2015-01-05 10:49:29 -0800

    Swift touching

--- Swift/SpinView.swift
+++ Swift/SpinView.swift
@@ -19,8 +19,11 @@ class SpinView : UIView {
     var zoomed : Bool = false
     var moved : Bool = false
     var startTouchPosition = CGPoint(x:0, y:0)
-    var initialDistance = CGFloat(0)
+    var initialDistance : Float = 0.0
     let spinClear : Float = 1.0
+    let HORIZ_SWIPE_DRAG_MIN : Float = 24.0
+    let VERT_SWIPE_DRAG_MAX : Float = 24.0
+    let TAP_MIN_DRAG : Float = 10.0
     
     let colors : [UInt8] = [
         0, 255, 0, 99,
@@ -172,5 +175,104 @@ class SpinView : UIView {
         glBindRenderbufferOES(GLenum(GL_RENDERBUFFER_OES), colorRenderbuffer);
         
         self.context?.presentRenderbuffer(Int(GL_RENDERBUFFER_OES))
+    }
+    
+    func distanceBetweenTwoPoints(fromPoint : CGPoint, toPoint : CGPoint) -> Float
+    {
+        let x : Float = Float(toPoint.x) - Float(fromPoint.x)
+        let y : Float = Float(toPoint.y) - Float(fromPoint.y)
+        println("distanceBetweenTwoPoints: toPoint = \(Int(toPoint.x)) \(Int(toPoint.y)), fromPoint = \(Int(fromPoint.x)) \(Int(fromPoint.y)), x = \(Int(x)), y = \(Int(y)), sqr = \(Int(x*x + y*y))")
+        return sqrtf(Float(x * x) + Float(y * y))
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        moved = false
+        switch (touches.count) {
+        case 1:
+            let touch = touches.anyObject() as UITouch
+            startTouchPosition = touch.locationInView(self)
+            initialDistance = -1
+        default:
+            // multi-touch
+            let touchArray = touches.allObjects
+            let touch1 = touchArray[0] as UITouch
+            let touch2 = touchArray[1] as UITouch
+            let initialDistance = distanceBetweenTwoPoints(touch1.locationInView(self), toPoint: touch2.locationInView(self))
+            println("Multi touch start with initial distance \(Int(initialDistance))")
+        }
+    }
+    
+    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
+        let touch1 = touches.allObjects[0] as UITouch
+        
+        if (zoomed && touches.count == 1) {
+            let pos : CGPoint = touch1.locationInView(self)
+            self.transform = CGAffineTransformTranslate(self.transform, pos.x - startTouchPosition.x, pos.y - startTouchPosition.y);
+            moved = true
+            return
+        }
+        
+        if ((initialDistance > 0) && (touches.count > 1)) {
+            let touch2 = touches.allObjects[1] as UITouch
+            let currentDistance = distanceBetweenTwoPoints(touch1.locationInView(self), toPoint: touch2.locationInView(self))
+            let movement = currentDistance - initialDistance
+            println("Touch moved: \(Int(movement))")
+            if (movement != 0) {
+                scale = scale * powf(2.0, movement / 100.0)
+                if (scale > 2.0) {
+                    scale = 2.0;
+                }
+                if (scale < 0.1) {
+                    scale = 0.1;
+                }
+            }
+        }
+    }
+
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        let touch1 = touches.allObjects[0] as UITouch
+        if (touches.count == 1) {
+            if (touch1.tapCount > 1) {
+                println("Double tap")
+                scale = 1.0
+                rotation = 3.0
+                return;
+            }
+            let currentTouchPosition : CGPoint = touch1.locationInView(self)
+            
+            let deltaX : Float = fabsf(Float(startTouchPosition.x) - Float(currentTouchPosition.x))
+            let deltaY : Float = fabsf(Float(startTouchPosition.y) - Float(currentTouchPosition.y))
+            // If the swipe tracks correctly.
+            if ((deltaX >= HORIZ_SWIPE_DRAG_MIN) && (deltaY <= VERT_SWIPE_DRAG_MAX))
+            {
+                // It appears to be a swipe.
+                let movement = startTouchPosition.x - currentTouchPosition.x;
+                if (movement < 0)
+                {
+                    println("Swipe Right");
+                    rotationSpeed = rotationSpeed + powf(2.0, Float(-movement / 100.0));
+                }
+                else
+                {
+                    rotationSpeed = rotationSpeed - powf(2.0, Float(movement / 100.0));
+                    println("Swipe Left");
+                }
+            }
+            else if (!moved && ((deltaX <= TAP_MIN_DRAG) && (deltaY <= TAP_MIN_DRAG)) )
+            {
+                // Process a tap event.
+                println("Tap");
+            }
+        }
+        else {
+            // multi-touch
+            let touch2 = touches.allObjects[1] as UITouch
+            let finalDistance = distanceBetweenTwoPoints(touch1.locationInView(self), toPoint: touch2.locationInView(self))
+            let movement = finalDistance - initialDistance
+            println("Final Distance: \(Int(finalDistance)), movement=\(Int(movement))")
+            if (movement != 0) {
+                println("Movement: \(Int(movement))")
+            }
+        }
     }
 }
